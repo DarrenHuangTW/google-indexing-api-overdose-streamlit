ea059163e45923743e2a1c3c98348646b7350e68
circleghost <circleghost0723@gmail.com>
2023-03-30 01:57:00 +0800

    Update index.py

--- index.py
+++ index.py
@@ -61,6 +61,6 @@ if submit_button and urls_input:
             st.error(f"{url}: 提交失敗")
         else:
             notify_time_str = response.get("urlNotificationMetadata", {}).get("latestUpdate", {}).get("notifyTime", "")
-            notify_time = datetime.strptime(notify_time_str.rstrip('Z'), "%Y-%m-%dT%H:%M:%S.%f").replace(tzinfo=timezone.utc)
+            notify_time = datetime.strptime(notify_time_str.split('.')[0].rstrip('Z'), "%Y-%m-%dT%H:%M:%S").replace(tzinfo=timezone.utc)
             notify_time = notify_time.replace(microsecond=0)
             st.success(f"{url}: 提交成功，提交時間為 {notify_time.strftime('%Y年%m月%d日 %H:%M')}")
