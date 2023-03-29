28597413c766b9cd4a00e441b492cb884c500f65
circleghost <circleghost0723@gmail.com>
2023-03-30 01:29:49 +0800

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,6 @@ import os
 import json
 import base64
 import datetime
-import httplib2
 import requests
 from google.oauth2 import service_account
 from googleapiclient.errors import HttpError
@@ -33,54 +32,33 @@ credentials = service_account.Credentials.from_service_account_info(
 # 建立 Google API 客戶端
 google_client = build("indexing", "v3", credentials=credentials)
 
-# 初始化 Google API 用戶端
-SCOPES = ["https://www.googleapis.com/auth/indexing"]
-ENDPOINT = "https://indexing.googleapis.com/v3/urlNotifications:publish"
+# Streamlit 應用介面
+st.title("Google 索引提交")
 
-credentials = service_account.Credentials.from_service_account_info(json_key_file, SCOPES)
-http = credentials.authorize(httplib2.Http())
-
-# 函數：提交 URLs 到 Google
-def submit_urls(urls):
-    response_messages = []
-    for url in urls:
-        url = url.strip()
-        if not url:
-            continue
-
-        content = f"""{{
-            "url": "{url}",
-            "type": "URL_UPDATED"
-        }}"""
-
-        response, content = http.request(ENDPOINT, method="POST", body=content)
-        content_json = json.loads(content.decode('utf-8'))
-
-        # 解析回應內容，生成易懂的訊息
-        if 'urlNotificationMetadata' in content_json:
-            notify_time_str = content_json['urlNotificationMetadata']['latestUpdate']['notifyTime']
-            notify_time = parser.isoparse(notify_time_str).replace(microsecond=0)
-            formatted_notify_time = notify_time.strftime('%Y年%m月%d日 %H:%M')
-            response_message = f"{url} | 提交成功，提交時間為 {formatted_notify_time}"
-        else:
-            response_message = f"{url}: 提交失敗"
-
-        response_messages.append(response_message)
-
-    return response_messages
-
-# Streamlit 介面
-st.title("Google 索引提交工具")
-
-urls = st.text_area("請在下方輸入需要提交的網址，每行一個")
+urls_input = st.text_area("請輸入要提交的網址（每行一個）")
 submit_button = st.button("提交")
 
-if submit_button:
-    if urls:
-        url_list = urls.split("\n")
-        responses = submit_urls(url_list)
+if submit_button and urls_input:
+    urls = urls_input.strip().split("\n")
 
-        for response in responses:
-            st.write(response)
-    else:
-        st.error("請輸入至少一個網址")
+    def submit_urls(urls):
+        responses = []
+        for url in urls:
+            try:
+                response = google_client.urlNotifications().publish(
+                    body={"url": url, "type": "URL_UPDATED"}
+                ).execute()
+                responses.append((url, response))
+            except HttpError as e:
+                responses.append((url, e))
+        return responses
+
+    responses = submit_urls(urls)
+
+    for url, response in responses:
+        if isinstance(response, HttpError):
+            st.error(f"{url}: 提交失敗")
+        else:
+            notify_time_str = response.get("urlNotificationMetadata", {}).get("latestUpdate", {}).get("notifyTime", "")
+            notify_time = datetime.datetime.fromisoformat(notify_time_str.rstrip('Z')).replace(microsecond=0)
+            st.success(f"{url}: 提交成功，提交時間為 {notify_time.strftime('%Y年%m月%d日 %H:%M')}")
